11588bb2297f1a6b2b52f42a0e64e95f35fdc0da
namudak <sbdak7@gmail.com>
2015-10-06 22:16:38 +0900

    Debugged Stopwatch mode_run/_stop/_reset

--- app/src/main/java/com/sb/android/myapplication/thread/StopWatchActivity.java
+++ app/src/main/java/com/sb/android/myapplication/thread/StopWatchActivity.java
@@ -2,7 +2,10 @@ package com.sb.android.myapplication.thread;
 
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -10,24 +13,40 @@ import android.widget.Toast;
 
 import com.sb.android.myapplication.R;
 
+
 public class StopWatchActivity extends AppCompatActivity {
 
-    private boolean mRunning = true;
+    private final int MODE_RUN= 1, MODE_STOP= 2, MODE_RESET= 3;
+    private int mRunning= MODE_RUN;
 
     private TimerTask mTask = new TimerTask();
-    private TextView mDisplay = (TextView) findViewById(R.id.textView);
+    private TextView mDisplay;
     private int mMils = 0;
     private int mSecs = 0;
     private int mMins = 0;
     private int mHour = 0;
 
-    private final String LAPSETIME= "%d:%d:%d:%d";
+    private final String LAPSETIME= "%04d:%02d:%02d:%02d";
     private final int MILUNIT= 1000, SECUNIT= 60, MINUNIT= 60;
     
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stop_watch);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        mDisplay= (TextView) findViewById(R.id.textView);
 
         Button b1 = (Button) findViewById(R.id.start_button);
         b1.setOnClickListener(startTimer);
@@ -36,51 +55,35 @@ public class StopWatchActivity extends AppCompatActivity {
         b2.setOnClickListener(stopTimer);
 
         Button b3 = (Button) findViewById(R.id.reset_button);
-        b2.setOnClickListener(resetTimer);
+        b3.setOnClickListener(resetTimer);
 
     }
 
     private View.OnClickListener startTimer = new View.OnClickListener() {
         public void onClick(View v) {
-            if(!mRunning){
-                startWatch();
-            }
-            else if(mRunning){
-                try{
-                    mTask.execute(""); //start the task
-                }
-                catch(Exception e1){
+            if(mRunning== MODE_RUN) {
+                try {
+                    mTask.execute("");
+                } catch (Exception e1) {
                     Toast.makeText(StopWatchActivity.this,
                             "Error starting: " + e1, Toast.LENGTH_LONG).show();
                 }
+            } else {
+                mRunning= MODE_RUN;
             }
         }
     };
 
     private View.OnClickListener stopTimer = new View.OnClickListener() {
         public void onClick(View v) {
-            if(mRunning){
-                stopWatch();
-            }
+            mRunning= MODE_STOP;
         }
     };
     private View.OnClickListener resetTimer = new View.OnClickListener() {
         public void onClick(View v) {
-            if(mRunning){
-                resetWatch();
-            }
+            mRunning= MODE_RESET;
         }
     };
-    public void startWatch(){
-        mRunning = true;
-    }
-    public void stopWatch(){
-        mRunning= false;
-    }
-    public void resetWatch(){
-        mRunning= false;
-        mDisplay.setText(String.format(LAPSETIME, 0, 0, 0, 0));
-    }
     /**
      * AsyncTask for stopwatch timer
      */
@@ -90,32 +93,42 @@ public class StopWatchActivity extends AppCompatActivity {
         protected String doInBackground(String... params) {
             while (true) {
 
-                while (mMils < MILUNIT && mRunning) {
+                while (mMils < MILUNIT) {
 
-                    mRunning = true;
-                    mMils++;
+                    if(mRunning!= MODE_STOP) {
+                        if(mRunning== MODE_RESET){
+                            mMils= mSecs= mMins= mHour= 0;
+                            this.publishProgress(String.format(
+                                    LAPSETIME, mMils, mSecs, mMins, mHour
+                            ));
+                            mRunning= MODE_RUN;
+                        } else {
+                            mMils++;
 
-                    try {
-                        Thread.sleep(1);
-                    } catch (Exception e1) {
-                        System.out.println(e1);
-                    }
+                            try {
+                                Thread.sleep(1);
+                            } catch (Exception e1) {
+                                System.out.println(e1);
+                            }
 
-                    if (mMils == MILUNIT) {
-                        mMils = 0;
-                        mSecs++;
-                    }
-                    if (mSecs == SECUNIT) {
-                        mSecs = 0;
-                        mMins++;
-                    }
-                    if (mMins == MINUNIT) {
-                        mHour++;
-                        mMins = 0;
-                    }
+                            if (mMils == MILUNIT) {
+                                mMils = 0;
+                                mSecs++;
+                            }
+                            if (mSecs == SECUNIT) {
+                                mSecs = 0;
+                                mMins++;
+                            }
+                            if (mMins == MINUNIT) {
+                                mHour++;
+                                mMins = 0;
+                            }
 
-                    this.publishProgress(String.format(
-                            LAPSETIME, mMils, mSecs, mMins, mHour));
+                            this.publishProgress(String.format(
+                                    LAPSETIME, mMils, mSecs, mMins, mHour
+                            ));
+                        }
+                    }
                 }
             }
         }
